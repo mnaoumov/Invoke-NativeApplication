9a0c9e6fda6bf7709fa675d5c10bfee07e0ac8e0
Michael Naumov <michael.naumov@volia-software.com>
2015-04-05 01:03:43 +0300

    Add first version. Blogged https://mnaoumov.wordpress.com/2015/01/11/execution-of-external-commands-in-powershell-done-right/

---
+++ exec.ps1
@@ -0,0 +1,35 @@
+function exec
+{
+    param
+    (
+        [ScriptBlock] $ScriptBlock,
+        [string] $StderrPrefix = "",
+        [int[]] $AllowedExitCodes = @(0)
+    )
+ 
+    $backupErrorActionPreference = $script:ErrorActionPreference
+ 
+    $script:ErrorActionPreference = "Continue"
+    try
+    {
+        & $ScriptBlock 2>&1 | ForEach-Object -Process `
+            {
+                if ($_ -is [System.Management.Automation.ErrorRecord])
+                {
+                    "$StderrPrefix$_"
+                }
+                else
+                {
+                    "$_"
+                }
+            }
+        if ($AllowedExitCodes -notcontains $LASTEXITCODE)
+        {
+            throw "Execution failed with exit code $LASTEXITCODE"
+        }
+    }
+    finally
+    {
+        $script:ErrorActionPreference = $backupErrorActionPreference
+    }
+}
