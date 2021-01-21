75b72a9e27d068c1988e64afbce06d3d5408a3c9
Michael Kriese <michael.kriese@visualon.de>
2021-01-21 12:14:41 +0100

    fix: add braces

--- exec.ps1
+++ exec.ps1
@@ -13,7 +13,7 @@ function Invoke-NativeApplication
     $ErrorActionPreference = "Continue"
     try
     {
-        Write-Verbose 'Executing native application with parameters: {0}' -f  [PSCustomObject] $ArgumentList
+        Write-Verbose ('Executing native application with parameters: {0}' -f [PSCustomObject] $ArgumentList)
         if (Test-CalledFromPrompt)
         {
             $wrapperScriptBlock = { & $ScriptBlock @ArgumentList }
@@ -30,8 +30,8 @@ function Invoke-NativeApplication
             }
         if ((-not $IgnoreExitCode) -and (Test-Path -Path Variable:LASTEXITCODE) -and ($AllowedExitCodes -notcontains $LASTEXITCODE))
         {
-            throw 'Native application with parameters {0} failed at {1} with exit code {2}' -f
-                ([PSCustomObject] $ArgumentList), (Get-PSCallStack -ErrorAction:SilentlyContinue)[1].Location, $LASTEXITCODE
+            throw ('Native application with parameters {0} failed at {1} with exit code {2}' -f
+                ([PSCustomObject] $ArgumentList), (Get-PSCallStack -ErrorAction:SilentlyContinue)[1].Location, $LASTEXITCODE)
         }
     }
     finally
